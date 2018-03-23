20ed6283769425f053555297a32ae718fdf6e0d2
teenmeem <Mubashir@example.com>
2018-03-23 10:14:20 +0500

    Library list using redux working

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+    componentWillMount() {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(this.props.libraries);
+
+    }
+
+    renderRow(library) {
+        console.log(library);
+        return (
+            <ListItem library={library} />
+        );
+    }
+
     render() {
-        console.log(this.props);  // returned from mapstatetoprops
-        return;
+        console.log(this.props);
+ //console.log(this.dataSource);
+        return (
+            <ListView
+              dataSource={this.dataSource}
+              renderRow={this.renderRow}
+            />
+        );
     }
 }
+
 const mapStateToProps =  state => {
-    return {libraries: state.libraries};
+    return {libraries: state.libraries}; // libraries ket is returned as a prop in LibraryListClass
 };
 
 export default connect(mapStateToProps)(LibraryList);
